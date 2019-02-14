e5edcbab612d4d39938d38c0c6421c9fa560cf51
hscheina <37668146+hscheina@users.noreply.github.com>
2019-02-13 21:15:21 -0600

    updated todos to include forEach and functions

--- Arrays/todos.js
+++ Arrays/todos.js
@@ -1,14 +1,39 @@
-var todos = [];
+var todos = ["buy new turtle"];
 window.setTimeout(function() {
   var input = prompt("What would you like to do?");
   while (input !== "quit") {
 	  	if (input === "list") {
-	  	console.log(todos);
+	  		listTodos();
 	  }
 	  else if (input === "new") {
-	  	var newTodo = prompt("Enter new to-do");
-	  	todos.push(newTodo);
+	  		addTodo();
+	  }
+	  else if (input === "delete") {
+	  		deleteTodo();
 	  }
 	  var input = prompt("What would you like to do?");
+  }
+
+  function listTodos() {
+  	console.log("**********");
+	todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+	console.log("**********");
+  }
+
+  function addTodo() {
+  	var newTodo = prompt("Enter new to-do");
+  	todos.push(newTodo);
+  	console.log("Added todo");
+  }
+
+  function deleteTodo() {
+  	//ask for index of todo to be deleted
+  	var index = prompt("Enter index of todo to delete.");
+  	//delete that todo
+  	//splice()
+  	todos.splice(index,1);
+  	console.log("Deleted todo");
   }
 }, 500);
